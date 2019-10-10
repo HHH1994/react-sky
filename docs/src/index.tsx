e48165725ddc158f1175dadf806a1c1571dccd2a
lvfeifei <574818075@qq.com>
2019-10-10 19:56:13 +0800

    测试github page

--- docs/src/index.tsx
+++ docs/src/index.tsx
@@ -2,11 +2,13 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 
-if (process.env.NODE_ENV == 'production') {
-  window.server = 'https://lvpangpang.github.io/react-sky/demos/dist/index.html#/';
-} else {
+
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV == 'development') {
   window.server = 'http://127.0.0.1:8081#/';
-}
+} else {
+  window.server = 'https://lvpangpang.github.io/react-sky/demos/dist/index.html#/';
+}  
 ReactDOM.render(
   <App />, 
   document.getElementById('app')
