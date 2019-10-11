0e615d352829dc6e9b069f95a6865db54835ff09
lvfeifei <574818075@qq.com>
2019-10-11 15:25:18 +0800

    动态配置serverName

--- docs/src/index.tsx
+++ docs/src/index.tsx
@@ -3,7 +3,7 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 
 if (process.env.NODE_ENV === 'production') {
-  (window as any).serverName = 'https://lvpangpang.github.io/react-sky/docs/dist/index.html#';
+  (window as any).serverName = 'https://lvpangpang.github.io/react-sky/demos/dist/index.html#';
 } else {
   (window as any).serverName = 'http://127.0.0.1:8082#';
 }
